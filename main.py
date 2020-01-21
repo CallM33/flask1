5c55f00d43bd27361ea83f6a9b3465d9c36e4f17
Nils <thepewpew2004@gmail.com>
2020-01-21 10:43:09 +0000

    did some changes in code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,14 +8,28 @@ def index():
 
 @app.route('/home')
 def home():
-  return render_template('home.html')
+  return render_template("home.html", aktiva_lapa = 'home')
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template("about.html")
 
 @app.route('/contact')
 def contact():
-  return render_template('contact.html', phone = 33333333)
+  return render_template('contact.html',phone=12345678)
+
+@app.route('/params')
+def params ():
+  args = request.args
+  for key, value in args.items():
+    print(f"{key}.{value}")
+  return args
+
+@app.route('/params_table')
+def params_table ():
+  args = request.args
+  return render_template('params_table.html', args = args)
+
 if __name__ =='__main__':
-  app.run(host = '0.0.0.0', port = 6969, threaded = True, debug = True)
+  app.run(host = '0.0.0.0', port = 5125, threaded = True, debug = True)
+#ieej heroku
