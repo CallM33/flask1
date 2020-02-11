bd0cdf6749293c13bb0c805705341c2f5845c97a
[Students] Nils Zirins <83DNZirins@rvt.lv>
2020-02-11 08:03:55 +0200

    changed up main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -8,28 +9,47 @@ def index():
 
 @app.route('/home')
 def home():
-  return render_template("home.html", aktiva_lapa = 'home')
+  return render_template('home.html')
 
 @app.route('/about')
-def about():
-  return render_template("about.html")
+def about_html():
+  return render_template('about.html')
 
 @app.route('/contact')
-def contact():
-  return render_template('contact.html',phone=12345678)
+def contacts_html():
+  return render_template('contact.html', phone = "22140077")
 
 @app.route('/params')
-def params ():
-  args = request.args
-  for key, value in args.items():
-    print(f"{key}.{value}")
-  return args
+def params():
+  return request.args
 
-@app.route('/params_table')
-def params_table ():
-  args = request.args
-  return render_template('params_table.html', args = args)
+@app.route('/post', methods = ['POST'])
+def post():
+  return request.get_json()
 
-if __name__ =='__main__':
-  app.run(host = '0.0.0.0', port = 5125, threaded = True, debug = True)
-#ieej heroku
+@app.route('/read_file')
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route('/write_file', methods = ['POST'])
+def write_to_file():
+  content_type = request.content_type
+  if content_type == 'application/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line {contentJSON['data']} to file."
+  else:
+    return f"Content type {content_type} is not supported!"
+  
+@app.route('/file', methods = ['GET', 'POST'])
+def workFile():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Method {request.method} is not supported!"
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5020, debug=True)
